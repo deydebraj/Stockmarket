bd811e5ca749fb2b82c0db16fb30637488877d1f
deydebraj <35585013+deydebraj@users.noreply.github.com>
2019-07-30 13:16:06 -0400

    Updates

---
+++ precache-manifest.92645b3a54ce95e7ae695d4aa382d6e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ed1627817c72529cb7f9b99863e857",
+    "url": "/Stockmarket/index.html"
+  },
+  {
+    "revision": "c992a72daefe0ef880fc",
+    "url": "/Stockmarket/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b7a4d4326cfd05c88482",
+    "url": "/Stockmarket/static/css/main.79e34894.chunk.css"
+  },
+  {
+    "revision": "c992a72daefe0ef880fc",
+    "url": "/Stockmarket/static/js/2.06f7f4db.chunk.js"
+  },
+  {
+    "revision": "b7a4d4326cfd05c88482",
+    "url": "/Stockmarket/static/js/main.9109ac13.chunk.js"
+  },
+  {
+    "revision": "7eb632675891907df37e",
+    "url": "/Stockmarket/static/js/runtime~main.671b11cb.js"
+  },
+  {
+    "revision": "30ca908c21e20078c05a19293391cae5",
+    "url": "/Stockmarket/static/media/left.30ca908c.jpg"
+  },
+  {
+    "revision": "fd989d416f6fd5a4145111499cdca64a",
+    "url": "/Stockmarket/static/media/right.fd989d41.jpg"
+  }
+]);
